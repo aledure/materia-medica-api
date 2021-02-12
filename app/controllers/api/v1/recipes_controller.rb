b831185b80a7e8a31bdf5152eefbc94b4d30b57a
aledure <mandrew.ledure@gmail.com>
2021-02-12 13:34:44 -0600

    Updating repo to keep only up-to-date production version

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::RecipesController < ApplicationController
+
+    def index
+        @recipe = Recipe.all
+        render json: @recipes
+    end
+
+    def show
+        render json: @recipes
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+            render json: @recipe, status: created
+        else
+            render json: @recipe.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @recipe.update(recipe_params)
+            render json: @recipe
+        else
+            render json: @recipe.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @recipe.destroy
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:body, :plant_id, :user_id)
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :users do
+        post :login
+        post :create
+        delete :logout
+        get :me
+      end
+      namespace :plants do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+        get :get_upload_credentials
+      end
+      namespace :recipes do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+      end
+    end
+  end
+end
